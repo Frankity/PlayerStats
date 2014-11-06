2981dcade5640f8bb3d22483425882029d148053
Douglas Brunal <frankithy@gmail.com>
2014-11-06 13:43:20 -0500

    added the word black color range , all credits to MAGIC

--- player.py
+++ player.py
@@ -13,7 +13,7 @@ def players(word, word_eol, userdata):
     if command == "player":
         response = urllib2.urlopen('http://api.emulatornexus.com/v1/rome/persona/'+ name +'/stats')
         jdata = json.load(response)
-        rank = '\002' + 'Rank: ' + '\002' + '\00307' + str(jdata['data']['rank']) + '\00302' 
+        rank = '\002' + '\00301' + 'Rank: ' + '\002' + '\00307' + str(jdata['data']['rank']) + '\00302' 
         score = '\002' + '\00301' + 'Score: ' + '\00301' + '\002' + '\00307' + str(jdata['data']['score']) + '\00302' 
         kills = '\002' + '\00301' + 'Kills: ' + '\00301' + '\002' + '\00307' + str(jdata['data']['kills']) + '\00302' 
         deaths = '\002' + '\00301' + 'Deaths: ' + '\00301' + '\002' + '\00307' + str(jdata['data']['deaths']) + '\00302' 
